d9549345bf5a2db8a26d8d2729e97be15d4fff0c
NSnietol <nilson-sn@outlook.com>
2018-09-08 20:51:31 -0500

    Fin de la app clima

--- Nodejs-de-cero-a-experto/05-clima-app/app.js
+++ Nodejs-de-cero-a-experto/05-clima-app/app.js
@@ -1,10 +1,19 @@
 const argv = require('./config/yargs').argv;
 const lugar = require('./data/lugar');
-
+const clima = require('./clima/weather');
 
 lugar.getDescripcion(argv.direccion).
 then((result) => {
-    console.log(result);
+
+    clima.getClima(result).then((resultado) => {
+
+        console.log(`La temperatura de ${result.direccion} es ${resultado} C`);
+
+    }).catch((error) => {
+
+        console.log(error);
+    });
+
 
 }).catch((error) => {
 
--- Nodejs-de-cero-a-experto/05-clima-app/clima/weather.js
+++ Nodejs-de-cero-a-experto/05-clima-app/clima/weather.js
@@ -1,10 +1,15 @@
 const axios = require('axios');
 
+const key = 'ae8c3550109bd65e6f959d67baf50a79';
 
 const getClima = async(coord) => {
 
-    result = await axios.get(`api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lng}`);
+    result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coord.latitud}&lon=${coord.longitud}&units=metric&appid=${key}`);
 
+    return result.data.main.temp;
 
+}
 
+module.exports = {
+    getClima
 }
