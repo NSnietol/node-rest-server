4d3876aa31a8378837545431c5024de137ea1745
Nilson Nieto López <nilson-sn@outlook.com>
2018-09-13 13:41:33 -0500

    Pasó algo

--- curso-udemy /FileSystem/readFileSync.js
+++
@@ -1,8 +0,0 @@
-let fs = require('fs');
-
-
-console.log(fs.readFileSync('test/archivo.txt','UTF-8'));
-
-var conf = JSON.parse(fs.readFileSync('test/package.json','UTF-8'));
-
-console.log(conf);
--- curso-udemy /FileSystem/readStream.js
+++
@@ -1,6 +0,0 @@
-let fs = require('fs');
-
-var total = "";
-
-// Se utiliza para leer archivoos grandes en secciones
-var stream = fs.createReadStream('')
