6f09a0b901910863fddbc7165c4e6b7630644ecd
Nilson Nieto López <nilson-sn@outlook.com>
2018-10-06 19:56:38 -0500

    update config.js despliege

--- Nodejs-de-cero-a-experto/07-rest-server/server/config/config.js
+++ Nodejs-de-cero-a-experto/07-rest-server/server/config/config.js
@@ -2,12 +2,9 @@
 //================PUERTO=================
 process.env.PORT = process.env.PORT || 3000;  
 
-
 //================ENTORNO=================
 
 process.env.NODE_ENV =  process.env.NODE_ENV || 'dev'
-
-
 
 //===================Base de datos ============
 
@@ -22,6 +19,5 @@ process.env.urlDB = urlDB;
 process.env.CADUCIDAD_TOKEN ='1h';
 
 //===================SEED DEL TOKEN ============
-
 
 process.env.SEED= process.env.SEED || 'D/E/V_S/E/C/R/E/T_2/0/1/8_0/9_1/8';
