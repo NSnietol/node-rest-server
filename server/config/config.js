79c3863dce0ed1a04fed167d893d96b85afdadcc
Nilson Nieto López <nilson-sn@outlook.com>
2018-10-07 19:34:23 -0500

    Google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -20,3 +20,9 @@ process.env.CADUCIDAD_TOKEN ='1h';
 //===================SEED DEL TOKEN ============
 
 process.env.SEED= process.env.SEED || 'D/E/V_S/E/C/R/E/T_2/0/1/8_0/9_1/8';
+
+
+
+//==================GOOGLE CLIENT=================
+process.env.CLIENT_ID = process.env.CLIENT_ID || "446350097880-5t7k6n37aibf8otk14hdlovp2t74vtu8.apps.googleusercontent.com";
+
