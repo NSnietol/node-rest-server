d63ce735d7000e0e46bd0e30b9359f986d270015
NSnietol <nilson-sn@outlook.com>
2018-10-18 22:09:44 -0500

    controlador categorias y productos lista

--- Nodejs-de-cero-a-experto/07-rest-server/server/controladores/index.js
+++ Nodejs-de-cero-a-experto/07-rest-server/server/controladores/index.js
@@ -9,4 +9,5 @@ app.use(require('./login.js'));
 
 app.use(require('./controlador_usuario'));
 app.use(require('./controlador_categoria'));
+app.use(require('./controlador_producto'));
 module.exports = app;
